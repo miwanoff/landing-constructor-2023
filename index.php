3e9d5c1651835ef7e2ce0c4c96e86d34c2e51aa0
miwanoff <marina.iva.noffa@gmail.com>
2023-02-05 15:21:08 +0200

    Add creation landing page

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ $header = new Header("Заголовок Лендінгу");
 $text = new Text("Це лендинг");
 $model = new Model([$header, $text], "Лендінг");
 $str_land = $model->generate(); // генерація тексту лендинга
-echo $str_land;
+//echo $str_land;
+
+$dir = "landing";
+mkdir($dir); // створення каталогу за вказаним шляхом
+$f = fopen("{$dir}/index.html", "w+"); // створення файлу лендингу за вказаним шляхом
+fwrite($f, $str_land); // запис в файл коду лендинга
+fclose($f);
+echo "Лендинг успішно створено!";
